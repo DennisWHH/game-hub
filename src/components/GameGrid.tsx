fa9d23de7b0af10b0640ddb23d4323776a3ebdce
Dennis Wojtyna <wojtyna.dennis@gmail.com>
2024-12-27 22:38:21 +0100

    Building Game Cards + implement Spinner

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -7,12 +8,16 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      {isLoading && <Text>...fetching games</Text>}
-      <ul>
+      {isLoading && <Spinner size="xl" />}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="20px"
+        spacing={10}
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
