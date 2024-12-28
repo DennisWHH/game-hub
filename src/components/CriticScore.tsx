a0c7d795f0debddd6b33001c9b3e0e7e29ef59b8
Dennis Wojtyna <wojtyna.dennis@gmail.com>
2024-12-28 23:26:00 +0100

    displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Badge, Text } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 90 ? "green" : score > 80 ? "yellow" : "grey";
+
+  return <Badge colorScheme={color}>{score}</Badge>;
+};
+
+export default CriticScore;
