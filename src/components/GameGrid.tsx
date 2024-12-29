01432f811410b8d52b7e8af0bc4f9c1e924229d8
Dennis Wojtyna <wojtyna.dennis@gmail.com>
2024-12-29 21:45:00 +0100

    display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
@@ -15,7 +15,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="20px"
-        spacing={10}
+        spacing={5}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
